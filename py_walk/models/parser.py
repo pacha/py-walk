9191fcdbbe0f09d02403015496b315031b22a87d
Andres Sopena Perez <andres.sopena@trivago.com>
2023-11-15 22:01:20 +0100

    feat: support omitting base directory

--- py_walk/models/parser.py
+++ py_walk/models/parser.py
@@ -12,9 +12,9 @@ from .pattern import Pattern
 @dataclass
 class Parser:
     patterns: List[Pattern]
-    base_dir: Path
+    base_dir: Union[Path, None]
 
-    def __init__(self, glob_pattern_list: List[str], base_dir: Path):
+    def __init__(self, glob_pattern_list: List[str], base_dir: Union[Path, None]):
         self.patterns = []
         for glob_pattern in glob_pattern_list:
             log.debug("\n-- Regex building")
@@ -26,17 +26,22 @@ class Parser:
     def match(self, path: Union[Path, str]) -> bool:
         # check if path is a directory
         trailing_slash = path.endswith(os.sep) if isinstance(path, str) else False
-        full_path = self.base_dir / Path(path)
-        path_is_dir = full_path.is_dir() or trailing_slash
+        if self.base_dir:
+            full_path = self.base_dir / Path(path)
+            path_is_dir = full_path.is_dir() or trailing_slash
+        else:
+            path_is_dir = trailing_slash
 
-        # directory paths always have a trailing slash so the Pattern
-        # class doesn't have to access disk
-        try:
-            relative_path = full_path.relative_to(self.base_dir)
-        except ValueError:
-            raise PyWalkInputError(
-                f"Error: path '{path}' is not relative to base dir '{self.base_dir}'"
-            )
+        # get relative path
+        if self.base_dir:
+            try:
+                relative_path = full_path.relative_to(self.base_dir)
+            except ValueError:
+                raise PyWalkInputError(
+                    f"Error: path '{path}' is not relative to base dir '{self.base_dir}'"
+                )
+        else:
+            relative_path = Path(path)
 
         # match against each pattern
         matched_patterns: List[int] = []
--- tests/test_directory.py
+++ tests/test_directory.py
@@ -3,9 +3,10 @@ from subprocess import run
 import pytest
 
 from py_walk import get_parser_from_file
+from py_walk import get_parser_from_list
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def repo_path(tmp_path_factory):
     # create repository
     path = tmp_path_factory.mktemp("repo")
@@ -17,7 +18,7 @@ def repo_path(tmp_path_factory):
     return path
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def parser(repo_path):
     gitignore_path = repo_path / ".gitignore"
     return get_parser_from_file(gitignore_path)
@@ -55,3 +56,23 @@ def test_slash_in_pattern_but_not_in_path_dir_exists(repo_path, parser):
     git_result = bool(command.returncode == 0)
     lib_result = parser.match(path)
     assert git_result == lib_result
+
+def test_no_base_dir(repo_path, parser):
+    """When no base_dir is provided the library can't check if the provided path is a directory."""
+    path = "my_dir"
+    (repo_path / path).mkdir()
+    command = run(
+        ["git", "check-ignore", "--", path], cwd=repo_path, capture_output=True
+    )
+    git_result = bool(command.returncode == 0)
+
+    # git will check that the path is actually a directory and return that there's a match
+    assert git_result is True
+
+    # with no base dir, the library won't be able to resolve the match correctly
+    parser_dir = get_parser_from_list(["my_dir/"])
+    assert parser_dir.match(path) is False
+
+    # however, with no trailing slash, the pattern will match directories and files
+    parser_file = get_parser_from_list(["my_dir"])
+    assert parser_file.match(path) is True
