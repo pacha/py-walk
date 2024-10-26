433e0ec80451979312c317d11ccb565dc9582f0b
Andres Sopena Perez <andres.sopena@trivago.com>
2024-10-26 16:27:54 +0200

    fix: make handler for logger a NullHandler to play nice with client code

--- py_walk/logs.py
+++ py_walk/logs.py
@@ -1,10 +1,4 @@
 import logging
 
 log = logging.getLogger("py_walk")
-log.setLevel(logging.INFO)
-
-# add handler
-formatter = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-log.addHandler(handler)
+log.addHandler(logging.NullHandler())
